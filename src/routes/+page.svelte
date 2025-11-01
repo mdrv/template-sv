ea17f33090963002216f9ce409130b65b597fce8
Umar Alfarouk <medrivia@gmail.com>
2025-11-01 17:09:18 +0700

    feat: Add Panda CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+import { css } from 'styled-system/css'
+</script>
+
+<div
+	class={css({
+		fontSize: '2xl',
+		fontWeight: 'bold',
+	})}
+>
+	Hello 🐼!
+</div>
--- svelte.config.ts
+++ svelte.config.ts
@@ -1,12 +1,21 @@
-import adapter from 'svelte-adapter-bun';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from 'svelte-adapter-bun'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 const config: import('@sveltejs/kit').Config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
-};
+		adapter: adapter(),
+		alias: {
+			'styled-system': './styled-system/*',
+		},
+		typescript: {
+			config: (config) => {
+				config.include.push('../styled-system')
+				return config
+			},
+		},
+	},
+}
 
-export default config;
+export default config
